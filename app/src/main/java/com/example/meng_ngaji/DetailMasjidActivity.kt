12473317762e743edd82602005cd85c9572d4d3e
kintanputia <kintanputia090101@gmail.com>
2021-10-12 00:36:04 +0700

    cm

---
+++ app/src/main/java/com/example/meng_ngaji/DetailMasjidActivity.kt
@@ -0,0 +1,15 @@
+package com.example.meng_ngaji
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailMasjidActivity :AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detailmasjid)
+    }
+
+
+
+}
