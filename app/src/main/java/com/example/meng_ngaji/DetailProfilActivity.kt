24fd7ff6817cc7c6121fe59652c50a4062759edc
Fathania Zulfani <fathaniazulfanii@gmail.com>
2021-10-12 21:05:14 +0700

    register, detail profil

---
+++ app/src/main/java/com/example/meng_ngaji/DetailProfilActivity.kt
@@ -0,0 +1,11 @@
+package com.example.meng_ngaji
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailProfilActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_profil)
+    }
+}
