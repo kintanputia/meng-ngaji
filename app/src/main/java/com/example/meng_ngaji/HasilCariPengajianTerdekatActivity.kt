37926c8839a03d1e24500c6f3953a7d5c101cbee
kintanputia <kintanputia090101@gmail.com>
2021-10-13 22:32:37 +0700

    intent pengajian

---
+++ app/src/main/java/com/example/meng_ngaji/HasilCariPengajianTerdekatActivity.kt
@@ -0,0 +1,11 @@
+package com.example.meng_ngaji
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HasilCariPengajianTerdekatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hasil_cari_pengajian_terdekat)
+    }
+}
--- app/src/main/java/com/example/meng_ngaji/PengajianFragment.kt
+++ app/src/main/java/com/example/meng_ngaji/PengajianFragment.kt
@@ -1,10 +1,13 @@
 package com.example.meng_ngaji
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
+import kotlinx.android.synthetic.main.fragment_pengajian.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,7 +19,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [PengajianFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class PengajianFragment : Fragment() {
+class PengajianFragment : Fragment(){
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -29,12 +32,19 @@ class PengajianFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_pengajian, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btn_terdekat.setOnClickListener(){
+            startActivity(Intent(context, HasilCariPengajianTerdekatActivity::class.java))
+        }
+        search.setOnClickListener(){
+            startActivity(Intent(context, HasilCariPengajianActivity::class.java))
+        }
     }
 
     companion object {
