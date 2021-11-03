cf5853ea2c68fd99a9af84237a6eb38101bc22a7
kintanputia <kintanputia090101@gmail.com>
2021-11-04 00:47:07 +0700

    recycler view

---
+++ app/src/main/java/com/example/meng_ngaji/TerjadwalAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.meng_ngaji
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.meng_ngaji.helper.Terjadwal
+import kotlinx.android.synthetic.main.list_masjid.view.lblJudul
+import kotlinx.android.synthetic.main.list_pengajian2.view.*
+
+class TerjadwalAdapter(private val context: PengajianFragment, private val arrayList: ArrayList<Terjadwal>) : RecyclerView.Adapter<TerjadwalAdapter.Holder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        return Holder(LayoutInflater.from(parent.context).inflate(R.layout.list_pengajian2,parent,false))
+    }
+
+    override fun getItemCount(): Int = arrayList!!.size
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+
+        holder.view.lblJudul.text = arrayList?.get(position)?.judul
+        holder.view.lblTanggal.text = arrayList?.get(position)?.tanggal
+        holder.view.lblJam.text = arrayList?.get(position)?.jam
+    }
+    class Holder(val view: View) : RecyclerView.ViewHolder(view)
+}
