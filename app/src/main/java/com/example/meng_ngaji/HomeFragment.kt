b9b6db56e9a6e0b65c05aea1345210bbd820df90
MFairuziIszamAziz <m.kholidatmi@gmail.com>
2021-11-11 02:44:15 +0700

    fragment home ke prayer

--- app/src/main/java/com/example/meng_ngaji/HomeFragment.kt
+++ app/src/main/java/com/example/meng_ngaji/HomeFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.FragmentTransaction
+import kotlinx.android.synthetic.main.fragment_home.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,6 +23,7 @@ class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var tvWaktuSolat : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,7 +38,19 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        val waktusolat = view.findViewById<TextView>(R.id.waktusolat)
+        waktusolat.setOnClickListener {
+            val prayerFragment = PrayerFragment()
+            val transaction: FragmentTransaction = requireFragmentManager().beginTransaction()
+            transaction.replace(R.id.layout,prayerFragment)
+            transaction.commit()
+
+        }
+
+
+        return view
     }
 
     companion object {
