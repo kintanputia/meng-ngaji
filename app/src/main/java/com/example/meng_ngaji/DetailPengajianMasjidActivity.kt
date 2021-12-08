73d6a40cee0a6dd73e96d7080fa357b1b57c9fda
kintanputia <kintanputia090101@gmail.com>
2021-12-09 01:03:53 +0700

    intent recycler view

--- app/src/main/java/com/example/meng_ngaji/DetailPengajianMasjidActivity.kt
+++ app/src/main/java/com/example/meng_ngaji/DetailPengajianMasjidActivity.kt
@@ -2,10 +2,18 @@ package com.example.meng_ngaji
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_detail_pengajian_masjid.*
 
 class DetailPengajianMasjidActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_NAME = "extra_name"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_pengajian_masjid)
+
+        nama_masjid.setText(intent.getStringExtra(EXTRA_NAME))
     }
 }
