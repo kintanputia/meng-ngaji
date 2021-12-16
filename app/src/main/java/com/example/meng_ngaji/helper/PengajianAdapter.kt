b712af00029c1567d99de83f99405195d3854672
kintanputia <kintanputia090101@gmail.com>
2021-12-16 07:37:51 +0700

    ambil data recycler tapi error

--- app/src/main/java/com/example/meng_ngaji/helper/PengajianAdapter.kt
+++ app/src/main/java/com/example/meng_ngaji/helper/PengajianAdapter.kt
@@ -1,34 +1,47 @@
 package com.example.meng_ngaji.helper
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.meng_ngaji.R
 import kotlinx.android.synthetic.main.list_masjid.view.*
+import retrofit2.Callback
 
-class PengajianAdapter(private val context: Context, private val arrayList: ArrayList<Masjid>, private var onItemClickCallback: OnItemClickCallback) : RecyclerView.Adapter<PengajianAdapter.Holder>(){
+class PengajianAdapter(private val arrayList: ArrayList<Masjid>, private var onItemClickCallback: OnItemClickCallback) : RecyclerView.Adapter<PengajianAdapter.PostViewHolder>(){
 
 //    private lateinit var onItemClickCallback: OnItemClickCallback
+    inner class PostViewHolder(itemView : View):RecyclerView.ViewHolder(itemView){
+        fun bind(postResponse : Masjid) {
+            with(itemView){
+                val text = "title : ${postResponse.namaMasjid}"
+                lblMasjid.text = text
+            }
+        }
+    }
 
     fun setOnItemClickCallback(
         onItemClickCallback: OnItemClickCallback) {
         this.onItemClickCallback = onItemClickCallback
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        return Holder(LayoutInflater.from(parent.context).inflate(R.layout.list_masjid,parent,false))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_masjid,parent,false)
+        return PostViewHolder(view)
+//        return Holder(LayoutInflater.from(parent.context).inflate(R.layout.list_masjid,parent,false))
     }
 
-    override fun getItemCount(): Int = arrayList!!.size
+    override fun getItemCount(): Int {
+        return arrayList.size
+    }
 
-    override fun onBindViewHolder(holder: Holder, position: Int) {
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
 
-        holder.view.lblMasjid.text = arrayList?.get(position)?.namaMasjid
-        holder.view.lblJudul.text = arrayList?.get(position)?.judul
-        holder.view.lblWaktu.text = arrayList?.get(position)?.waktu
-        holder.view.lblJarak.text = arrayList?.get(position)?.jarak
+//        holder.view.lblMasjid.text = arrayList?.get(position)?.namaMasjid
+//        holder.view.lblJudul.text = arrayList?.get(position)?.judul
+//        holder.view.lblWaktu.text = arrayList?.get(position)?.waktu
+//        holder.view.lblJarak.text = arrayList?.get(position)?.jarak
+        holder.bind(arrayList[position])
         holder.itemView.setOnClickListener() {
             onItemClickCallback
                 .onItemClick(arrayList[holder.adapterPosition])
---
+++ app/src/main/java/com/example/meng_ngaji/helper/RetrofitClient.kt
@@ -0,0 +1,17 @@
+package com.example.meng_ngaji.helper
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "https://meng-ngaji.herokuapp.com/"
+
+    val instance : ApiDataPengajian by lazy {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        retrofit.create(ApiDataPengajian::class.java)
+    }
+}
